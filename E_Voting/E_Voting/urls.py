ace4699914381f7a50fcd2e8110d96be53b6e436
Bryan-Keith Jaralve <jaralveb@ter.teradyne.com>
2019-06-15 23:45:27 +0800

    change app done

---
+++ E_Voting/E_Voting/urls.py
@@ -0,0 +1,42 @@
+"""E_Voting URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.HomePage.as_view(),name='home'),
+    path('ajax/login/', views.LoginViewAjax.as_view(
+        template_name='partial_login_ajax.html'),
+        name='login_ajax'),
+    path('ajax/logout/', views.logout_view_ajax, name='logout_ajax'),
+    path('registration/',include('registration.urls',namespace='registration')),
+    path('election/',include('election.urls',namespace='election')),
+    path('reporting/',include('reporting.urls',namespace='reporting')),
+    path('accounts/login/', views.LoginView.as_view(),
+        name='login'),
+    path('test/',
+        views.TestPage.as_view(),
+        name='test'
+    ),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
