eb635bf2290482c1c09212adb8873105fc7b15e7
Bryan-Keith Jaralve <jaralveb@ter.teradyne.com>
2019-06-22 16:04:54 +0800

    working

--- E_Voting/E_Voting/wsgi.py
+++ E_Voting/E_Voting/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'E_Voting.settings2.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'E_Voting.settings')
 
 application = get_wsgi_application()
